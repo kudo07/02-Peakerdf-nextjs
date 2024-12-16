2076e37a051801cc344408051ac6b0da6c3e0483
kudo07 <vknwl077@gmail.com>
2024-12-17 01:05:18 +0530

    [fileid]

---
+++ Components/PdfRenderer.tsx
@@ -0,0 +1,5 @@
+const PdfRenderer = () => {
+  return <div></div>;
+};
+
+export default PdfRenderer;
--- app/dashboard/[fileid]/page.tsx
+++ app/dashboard/[fileid]/page.tsx
@@ -1,5 +1,8 @@
+import ChatWrapper from '@/components/chat/ChatWrapper';
+import PdfRenderer from '@/components/PdfRenderer';
+import { db } from '@/db';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
-import { redirect } from 'next/navigation';
+import { notFound, redirect } from 'next/navigation';
 
 interface PageProps {
   params: { fileid: string };
@@ -11,7 +14,30 @@ const Page = async ({ params }: PageProps) => {
   const user = await getUser();
   if (!user || !user.id) redirect(`/auth-callback?origin=dashboard/${fileid}`);
   //   find the files for the particular user
-  return <div></div>;
+  const file = await db.file.findFirst({
+    where: {
+      id: fileid,
+      userId: user.id,
+    },
+  });
+  if (!file) notFound();
+  // epic
+  return (
+    <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
+      <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
+        {/* left sidebar @ main wrapper */}
+        <div className="flex-1 xl:flex">
+          <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+            {/* main area */}
+            <PdfRenderer />
+          </div>
+        </div>
+        <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
+          <ChatWrapper />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
